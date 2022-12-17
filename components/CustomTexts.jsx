537de869f05cd860ba13399a458dfe6a3e0a23f4
techpassel <techpasselorg@gmail.com>
2022-12-17 23:14:54 +0530

    Completed get started and what's new section

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion'
 import { textContainer, textVariant2 } from '../utils/motion';
 
 
-export const TypingText = ({ title, textStyles }) => (
+export const TypingText = ({ title, textStyles = "" }) => (
   <motion.p
     variants={textContainer}
     className={`font-normal text-[14px] text-secondary-white ${textStyles}`}
@@ -19,7 +19,7 @@ export const TypingText = ({ title, textStyles }) => (
   </motion.p>
 );
 
-export const TitleText = ({ title, textStyles }) => (
+export const TitleText = ({ title, textStyles = "" }) => (
   <motion.h2
     variants={textVariant2}
     initial="hidden"
